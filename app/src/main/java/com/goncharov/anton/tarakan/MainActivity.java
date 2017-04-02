1a784e0721b3d2982d5bd727e2009f0e8e51173a
Anton Goncharov <mail.antony@mail.ru>
2017-04-02 18:24:29 +0300

    Changed comment

--- app/src/main/java/com/goncharov/anton/tarakan/MainActivity.java
+++ app/src/main/java/com/goncharov/anton/tarakan/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Здесь будет код
+        //Здесь будет код игры
     }
 }
